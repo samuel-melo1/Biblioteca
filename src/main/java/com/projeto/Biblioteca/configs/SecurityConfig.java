4bb88fe1977f08e106441f8ddbc43344c272caeb
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-06-10 00:50:10 -0300

    biblioteca project

---
+++ src/main/java/com/projeto/Biblioteca/configs/SecurityConfig.java
@@ -0,0 +1,40 @@
+/* 
+package com.projeto.Biblioteca.configs;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+            .authorizeHttpRequests(requests -> requests
+                .requestMatchers("/login").permitAll()
+                .anyRequest().authenticated())
+            .formLogin(login -> login
+                .loginPage("/login")
+                .usernameParameter("email")
+                .passwordParameter("senha")
+                .permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    
+}
+*/
